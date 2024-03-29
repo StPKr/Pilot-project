eda6f5a968d3014bdc4dd3d53abf89164a0b7e5d
Public Name <137268124+StPKr@users.noreply.github.com>
2024-03-29 19:41:03 +0200

    test

--- src/lib.js
+++ src/lib.js
@@ -1,6 +1,7 @@
 // import { render, html } from '../node_modules/lit-html/lit-html.js';
-import {html, render} from 'https://esm.run/lit-html@1';
-import page from '../node_modules/page/page.mjs';
+// import page from '../node_modules/page/page.mjs';
+import { render, html } from '../lit-html/lit-html.js';
+import page from '../page/page.mjs';
 
 const root = document.querySelector('main');
 
