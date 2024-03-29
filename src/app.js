89d15c1e073911c565fe31aa0d821191d35c676b
Public Name <137268124+StPKr@users.noreply.github.com>
2024-03-29 13:53:15 +0200

    css

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ import { showForecastView } from './views/forecastView.js';
 import { showHomeView } from './views/homeView.js';
 
 page('/', showHomeView);
-page('/PH1', () => alert('PH1'));
-page('/PH2', () => alert('PH2'));
+page('/PH1', () => alert('PH1 - I do nothing yet'));
+page('/PH2', () => alert('PH2 - I also do nothing yet...'));
 page('/forecast', showForecastView);
 
 page.start()
--- src/views/homeView.js
+++ src/views/homeView.js
@@ -2,9 +2,9 @@ import { apiGet } from "../data/api.js";
 import { renderer, html, page } from "../lib.js";
 
 
-const homeTemplate = () => html`
+const homeTemplate = (onSubmit, onRandom) => html`
 <div class="container" id="current-day">
-    <h2>Welcome traveller</h2>
+    <h1>Welcome, traveller!</h1>
 </div>
 
 <div class="container" id="current-day">
@@ -25,7 +25,7 @@ const homeTemplate = () => html`
 
 export function showHomeView(ctx) {
     ctx.data = "TEST"
-    renderer(homeTemplate());
+    renderer(homeTemplate(onSubmit, onRandom));
 }
 
 export let result = {};
@@ -45,10 +45,20 @@ async function onSubmit(e) {
         alert('Please enter a valid number');
         e.target.reset();
         return
+    } // fix this check
+
+    if (lattitude > 90 || lattitude < -90) {
+        alert('Lattitude range is not valid. Please pick a value betwee -90 to 90!')
     }
+
+    if (longitude > 180 || longitude < -180) {
+        alert('Longitude range is not valid. Please pick a value betwee -180 to 180!')
+    }
+
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lattitude}&lon=${longitude}&appid=df4100685cdbaac6b9eda4bd1ed8f22a`
     result = await apiGet(url);
     page.redirect('/forecast');
+    e.target.reset();
 
 }
 
