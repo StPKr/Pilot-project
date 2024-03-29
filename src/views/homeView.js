4b96e40dbc9e73c5b1b7d719abb2cdc9e21924af
Public Name <137268124+StPKr@users.noreply.github.com>
2024-03-29 14:20:30 +0200

    functionality fixes

--- src/views/homeView.js
+++ src/views/homeView.js
@@ -13,8 +13,8 @@ const homeTemplate = (onSubmit, onRandom) => html`
 
 <div>
     <form @submit=${onSubmit}>
-        <label for="name">Lattitude:</label>
-        <input type="text" id="lattitude" name="lattitude"><br><br>
+        <label for="name">Latitude:</label>
+        <input type="text" id="latitude" name="latitude"><br><br>
         <label for="email">Longitude:</label>
         <input type="text" id="longitude" name="longitude"><br><br>
         <button type="submit">Submit</button>
@@ -32,30 +32,31 @@ export let result = {};
 async function onSubmit(e) {
     e.preventDefault();
     const formData = new FormData(e.target);
-    let { lattitude, longitude } = Object.fromEntries(formData);
+    let { latitude, longitude } = Object.fromEntries(formData);
 
-    if (lattitude.length < 1 && longitude.length < 1) {
-        alert('Please enter a value');
-        return
+    if (latitude.trim() === '' || longitude.trim() === '') {
+        alert('Please enter a value!');
+        return;
     }
-    lattitude = Number(lattitude);
+
+    latitude = Number(latitude);
     longitude = Number(longitude);
 
-    if (typeof (lattitude) != 'number' || typeof (longitude) != 'number') {
-        alert('Please enter a valid number');
+    if (isNaN(latitude) || isNaN(longitude)) {
+        alert('Please enter valid numbers for latitude and longitude!');
         e.target.reset();
-        return
-    } // fix this check
+        return;
+    }
 
-    if (lattitude > 90 || lattitude < -90) {
-        alert('Lattitude range is not valid. Please pick a value betwee -90 to 90!')
+    if (latitude > 90 || latitude < -90) {
+        alert('latitude range is not valid. Please pick a value betwee -90 to 90!')
     }
 
     if (longitude > 180 || longitude < -180) {
         alert('Longitude range is not valid. Please pick a value betwee -180 to 180!')
     }
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lattitude}&lon=${longitude}&appid=df4100685cdbaac6b9eda4bd1ed8f22a`
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=df4100685cdbaac6b9eda4bd1ed8f22a`
     result = await apiGet(url);
     page.redirect('/forecast');
     e.target.reset();
@@ -63,9 +64,9 @@ async function onSubmit(e) {
 }
 
 async function onRandom(e) {
-    const lattitude = (Math.random() * 180 - 90).toFixed(2);
+    const latitude = (Math.random() * 180 - 90).toFixed(2);
     const longitude = (Math.random() * 360 - 180).toFixed(2);
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lattitude}&lon=${longitude}&appid=df4100685cdbaac6b9eda4bd1ed8f22a`
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=df4100685cdbaac6b9eda4bd1ed8f22a`
     result = await apiGet(url);
     page.redirect('/forecast');
 }
